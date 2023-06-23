7793d5b8593b9a83fc1e49a5492a7dc78c2426aa
LeDrew2017 <33235594+LeDrew2017@users.noreply.github.com>
2023-06-22 20:48:29 -0400

    exynos9820-common: camera/provider: Uprev to 2.6
    Samsung's camera HAL 4.0 extends 2.6, so lets do the same here.

--- common.mk
+++ common.mk
@@ -55,7 +55,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.5-service_64.exynos9820 \
+    android.hardware.camera.provider@2.6-service_64.exynos9820 \
     libsensorndkbridge
 
 PRODUCT_PACKAGES += \
